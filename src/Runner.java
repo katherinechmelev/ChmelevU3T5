c0502f45ac836565fd381a38430d244a82ea79b2
student <bths.bths@edu>
2022-10-31 12:32:18 -0400

    initial

---
+++ src/Runner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+
+// CODE SEGMENT D
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+
+
+    }
+}
